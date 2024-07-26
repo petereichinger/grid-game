9f4d0535f479ed7760bdf9d6f24959c2bc0bf4c6
Peter Eichinger <peter.eichinger@gmail.com>
2024-07-26 23:27:11 +0200

    Test mesh primitive generation

--- src/height_grid/mesh_builder/mesh_data.rs
+++ src/height_grid/mesh_builder/mesh_data.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 
+#[derive(Clone, Default)]
 pub struct MeshData {
     pub positions: Vec<Vec3>,
     pub indices: Vec<u32>,
@@ -21,10 +22,10 @@ impl MeshData {
         self.indices.push(index_offset + 1);
         self.indices.push(index_offset + 2);
 
-        let a = (vertices[2] - vertices[0]).normalize_or(Vec3::Z);
-        let b = (vertices[1] - vertices[0]).normalize_or(Vec3::Z);
+        let a = (vertices[1] - vertices[0]).normalize_or(Vec3::X);
+        let b = (vertices[2] - vertices[0]).normalize_or(Vec3::Y);
 
-        let normal: [f32; 3] = b.cross(a).into();
+        let normal: [f32; 3] = a.cross(b).into();
 
         self.normals.extend(std::iter::repeat(normal).take(3));
 
@@ -51,13 +52,57 @@ impl MeshData {
         self.indices.push(index_offset + o5);
         self.indices.push(index_offset + o6);
 
-        let a = (vertices[2] - vertices[0]).normalize_or(Vec3::Z);
-        let b = (vertices[1] - vertices[0]).normalize_or(Vec3::Z);
+        let a = (vertices[2] - vertices[0]).normalize_or(Vec3::X);
+        let b = (vertices[1] - vertices[0]).normalize_or(Vec3::Y);
 
         let normal: [f32; 3] = a.cross(b).into();
 
         self.normals.extend(std::iter::repeat(normal).take(4));
         // TODO:: UVs
         self.uvs.extend(std::iter::repeat([0.0, 0.0]).take(4));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bevy::math::Vec3;
+
+    use super::MeshData;
+
+    #[test]
+    fn create_triangle_works() {
+        let mut mesh_data = MeshData::default();
+
+        mesh_data.create_triangle(&[Vec3::ZERO, Vec3::X, Vec3::Y]);
+
+        assert_eq!(mesh_data.positions.len(), 3);
+        assert_eq!(mesh_data.indices.len(), 3);
+        assert_eq!(mesh_data.uvs.len(), 3);
+        assert_eq!(mesh_data.normals.len(), 3);
+
+        assert_eq!(mesh_data.positions, &[Vec3::ZERO, Vec3::X, Vec3::Y]);
+        assert_eq!(mesh_data.indices, &[0, 1, 2]);
+        let normal: [f32; 3] = Vec3::Z.into();
+        assert_eq!(mesh_data.normals, &[normal, normal, normal]);
+    }
+
+    #[test]
+    fn create_quad_works() {
+        let mut mesh_data = MeshData::default();
+
+        mesh_data.create_quad(&[Vec3::ZERO, Vec3::X, -Vec3::Y, Vec3::X - Vec3::Y]);
+
+        assert_eq!(mesh_data.positions.len(), 4);
+        assert_eq!(mesh_data.indices.len(), 6);
+        assert_eq!(mesh_data.uvs.len(), 4);
+        assert_eq!(mesh_data.normals.len(), 4);
+
+        assert_eq!(
+            mesh_data.positions,
+            &[Vec3::ZERO, Vec3::X, -Vec3::Y, Vec3::X - Vec3::Y]
+        );
+        assert_eq!(mesh_data.indices, &[0, 2, 1, 1, 2, 3]);
+        let normal: [f32; 3] = Vec3::Z.into();
+        assert_eq!(mesh_data.normals, &[normal, normal, normal, normal]);
     }
 }
